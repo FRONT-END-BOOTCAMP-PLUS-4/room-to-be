2ae1cc388b93fa7164671ba9e32c373ce2e92b78
Jusang98 <rlatkdwn3379@gmail.com>
2025-05-13 16:22:28 +0900

    feat: 캔버스 초기화 + 방 구조/벽 생성

---
+++ app/simulator/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+
+import Room from './components/room/Room';
+import Lighting from './components/room/Lighting';
+import CameraController from './components/room/CameraController';
+import CameraButtons from './components/room/CameraButtons';
+import CenterMarker from './components/room/CenterMaker';
+
+export default function SimulatorPage() {
+  return (
+    <div className='w-full h-screen relative'>
+      <CameraButtons />
+
+      <Canvas
+        shadows
+        camera={{ position: [5, 5, 5], fov: 50 }}
+        className='w-full h-full'
+      >
+        <Suspense fallback={null}>
+          <Room
+            // 여기서 width, height는 미터단위고 처음 입력값 받아서 넘겨오기
+            width={4}
+            height={4}
+            wallTexture='/assets/images/testwall.jpg'
+            floorTexture='/assets/images/woodfloor.png'
+          />
+
+          <Lighting />
+          <CameraController width={4} height={4} />
+          <CenterMarker x={4 / 2} z={4 / 2} />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
