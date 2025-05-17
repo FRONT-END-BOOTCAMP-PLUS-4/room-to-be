a03b60190b4c840d03a0907b5f93a658b9389d20
“lbyul” <rlaquf7737@naver.com>
2025-05-17 20:27:05 +0900

    feat: 3D/2D 뷰 전환 기능을 FurnitureControllerBtn으로 이전
    - CameraButtons.tsx에서 뷰 전환 관련 로직을 FurnitureControllerBtn.tsx로 이전

--- app/simulator/components/furnitures/FurnitureControllerBtn.tsx
+++ app/simulator/components/furnitures/FurnitureControllerBtn.tsx
@@ -1,7 +1,28 @@
 'use client';
 
-import { useState } from "react";
-import Image from "next/image";
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useViewStore } from '@/stores/useViewStore';
+
+const VALID_ANGLES = [45, 135, 225, 315];
+const TOP_VIEW_ANGLES = [0, 90, 180, 270];
+
+function convertForm3DToTop(angle3D: number): number {
+  const index3D = VALID_ANGLES.indexOf(angle3D);
+  if (index3D !== -1) {
+    return TOP_VIEW_ANGLES[index3D];
+  }
+  return 0;
+}
+
+function convertFormTopTo3D(angleTop: number): number {
+  const indexTop = TOP_VIEW_ANGLES.indexOf(angleTop);
+  if (indexTop !== -1) {
+    return VALID_ANGLES[indexTop];
+  }
+
+  return 45;
+}
 
 interface FurnitureControllerBtnProps {
   icon?: string;
@@ -18,21 +39,36 @@ export default function FurnitureControllerBtn({
   height,
   onClick,
 }: FurnitureControllerBtnProps) {
+  const angle = useViewStore((s) => s.angle);
+  const setAngle = useViewStore((s) => s.setAngle);
+  const isTopView = useViewStore((s) => s.isTopView);
+  const setIsTopView = useViewStore((s) => s.setIsTopView);
+
   const [viewMode, setViewMode] = useState(text);
 
-  const handleClick = () => {
-    if (viewMode === "3D 뷰") {
-      setViewMode("2D 뷰");
-    } else if (viewMode === "2D 뷰") {
-      setViewMode("3D 뷰");
-    }
+  const isViewToggle = text === '3D 뷰' || text === '2D 뷰';
 
-    if (onClick) {
-      onClick();
+  useEffect(() => {
+    if (isViewToggle) {
+      setViewMode(isTopView ? '2D 뷰' : '3D 뷰');
+    }
+  }, [isTopView, isViewToggle]);
+
+  const handleClick = () => {
+    if (isViewToggle) {
+      if (isTopView) {
+        const new3DAngle = convertFormTopTo3D(angle);
+        setAngle(new3DAngle);
+        setIsTopView(false);
+        setViewMode('3D 뷰');
+      } else {
+        const newTopAngle = convertForm3DToTop(angle);
+        setAngle(newTopAngle);
+        setIsTopView(true);
+        setViewMode('2D 뷰');
+      }
     }
   };
-  
-  
 
   return (
     <div
@@ -44,15 +80,8 @@ export default function FurnitureControllerBtn({
         transition-colors duration-300 ease-in-out select-none
       `}
     >
-      {icon && (
-        <Image
-          src={icon}
-          alt="icon"
-          width={width}
-          height={height}
-        />
-      )}
-      {viewMode && <span className="text-[12px] text-white">{viewMode}</span>}
+      {icon && <Image src={icon} alt='icon' width={width} height={height} />}
+      {viewMode && <span className='text-[12px] text-white'>{viewMode}</span>}
     </div>
   );
 }
