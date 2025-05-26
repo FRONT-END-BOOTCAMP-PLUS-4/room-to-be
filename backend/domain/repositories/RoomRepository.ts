2a99172f8439ffc10fd663aa9407e32fa4129da9
Jusang98 <rlatkdwn3379@gmail.com>
2025-05-26 14:28:23 +0900

    feat: 방 save 함수 구현

--- backend/domain/repositories/RoomRepository.ts
+++ backend/domain/repositories/RoomRepository.ts
@@ -2,7 +2,7 @@ import { Room } from '@/backend/domain/entities/Room';
 
 export interface RoomRepository {
   findById(id: string): Promise<Room | null>;
-  saveRoom(room: Room): Promise<Room>;
+  save(room: Room): Promise<Room>;
   findByUserId(userId: string): Promise<Room[]>;
   deleteById(id: string): Promise<void>;
 }
--- backend/infra/db/models/prismaRoomRepository.ts
+++ backend/infra/db/models/prismaRoomRepository.ts
@@ -1,28 +1,51 @@
 import { Room } from '@/backend/domain/entities/Room';
 import { RoomRepository } from '@/backend/domain/repositories/RoomRepository';
-import { toDomainRoom, toPrismaRoom } from '@/backend/utils/mapper';
+import {
+  toDomainRoom,
+  toPrismaPlacedFurnitureCreateInput,
+} from '@/backend/utils/mapper';
 
 import { prisma } from '../prisma/prismaClient';
 
 export class PrismaRoomRepository implements RoomRepository {
   async findById(id: string): Promise<Room | null> {
-    const data = await prisma.room.findUnique({ where: { id } });
+    const data = await prisma.room.findUnique({
+      where: { id },
+      include: { furnitures: true }, //
+    });
+
     return data ? toDomainRoom(data) : null;
   }
 
-  async saveRoom(room: Room): Promise<Room> {
-    const data = await prisma.room.create({
-      data: toPrismaRoom(room),
+  async save(room: Room): Promise<Room> {
+    const created = await prisma.room.create({
+      data: {
+        name: room.name,
+        width: room.width,
+        height: room.height,
+        user_id: room.userId,
+        thumbnail_url: room.thumbnailUrl,
+        furnitures: {
+          create: room.furnitures.map(toPrismaPlacedFurnitureCreateInput),
+        },
+      },
+      include: {
+        furnitures: true,
+      },
     });
-    return toDomainRoom(data);
+
+    return toDomainRoom(created);
   }
+
   async findByUserId(userId: string): Promise<Room[]> {
     const data = await prisma.room.findMany({
       where: { user_id: userId },
+      include: { furnitures: true }, 
     });
 
     return data.map(toDomainRoom);
   }
+
   async deleteById(id: string): Promise<void> {
     await prisma.room.delete({
       where: { id },
