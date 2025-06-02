15f2553708e77f24c17e6c7f97dfb1b31998f857
“lbyul” <rlaquf7737@naver.com>
2025-06-02 21:49:28 +0900

    style: FurnitureController 스크롤바 추가

--- app/simulator/components/furnitures/FurnitureController.tsx
+++ app/simulator/components/furnitures/FurnitureController.tsx
@@ -66,7 +66,10 @@ export default function FurnitureController({
   );
 
   return (
-    <div className='w-[219px] px-[30px] py-[25px] rounded-[30px] bg-gradient-to-r from-white/10 to-black/20 backdrop-blur-md shadow-[0_0_15px_#00000026] flex flex-col items-center justify-center gap-4'>
+    <div
+      className='w-[219px] px-[30px] py-[25px] rounded-[30px] bg-gradient-to-r from-white/10 to-black/20 backdrop-blur-md shadow-[0_0_15px_#00000026] flex flex-col items-center gap-4 max-h-[calc(100vh-60px)]
+      overflow-y-auto scrollbar-none [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'
+    >
       {/* 저장, 나가기 버튼 */}
       <ActionButtons mode={mode} onSaveClick={onSaveClick} />
 
