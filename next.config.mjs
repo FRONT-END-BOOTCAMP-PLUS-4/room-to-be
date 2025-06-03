2112272788e65da93e18fda4dff630536f35455e
Jusang98 <rlatkdwn3379@gmail.com>
2025-06-04 03:58:03 +0900

    fix: next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname 대체
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'lh3.googleusercontent.com', // 구글
-      'ssl.pstatic.net', // 네이버
-      'k.kakaocdn.net', // 카카오
-    ],
+    domains: ['lh3.googleusercontent.com', 'ssl.pstatic.net', 'k.kakaocdn.net'],
     remotePatterns: [
       {
         protocol: 'https',
