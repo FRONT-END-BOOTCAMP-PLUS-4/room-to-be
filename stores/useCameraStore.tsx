507c7e5e5ef24e8eb92de9a9b20e0f936531974a
Jusang98 <rlatkdwn3379@gmail.com>
2025-06-04 17:37:04 +0900

    feat: 카메라 Zustand 상태를 localStorage에 자동으로 저장

--- stores/useCameraStore.tsx
+++ stores/useCameraStore.tsx
@@ -1,11 +1,19 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface CameraState {
   position: [number, number, number];
   setPosition: (pos: [number, number, number]) => void;
 }
 
-export const useCameraStore = create<CameraState>((set) => ({
-  position: [0, 0, 0],
-  setPosition: (pos) => set({ position: pos }),
-}));
+export const useCameraStore = create(
+  persist<CameraState>(
+    (set) => ({
+      position: [5, 3, 5],
+      setPosition: (pos) => set({ position: pos }),
+    }),
+    {
+      name: 'camera-storage',
+    },
+  ),
+);
